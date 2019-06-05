9540c257bc2b9e15453f08ddc961d414ccd46dc8
gurpreetsinghmatharoo <gurpreetsingh793@gmail.com>
2019-06-05 17:46:43 +0530

    u

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+// Canvas
+let canvas = document.getElementById("funcanvas");
+let main = document.getElementById("main");
+
+if (canvas.getContext) {
+    let ctx = canvas.getContext("2d");
+
+    // Size
+    canvas.width = main.offsetWidth;
+    canvas.height = 160;
+
+    // Fill
+    //ctx.fillStyle = "rgb(1, 0.8, 0.5, 1)";
+    //ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Public
+    let frameRate = 30;
+
+    // Create
+    let Create = () => {
+        // Object class
+        class Obj {
+            constructor (x, y, width, height) {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+            }
+
+            move (xadd, yadd) {
+                this.x += xadd;
+                this.y += yadd;
+            }
+
+            outOfBounds () {
+                return this.bbox_left < 0 || this.bbox_top < 0 || this.bbox_right >= 
+            }
+
+            update () {
+                this.bbox_left = x;
+                this.bbox_top = y;
+                this.bbox_right = x + width;
+                this.bbox_bottom = y + height;
+                this.bbox_x = x + width/2;
+                this.bbox_y = y + height/2;
+            }
+        }
+
+        // Player
+        let player = new Obj(32, 64);
+        player.gravity = 0;
+    }
+
+    // Step
+    let Step = () => {
+        // Player gravity
+
+    }
+
+    setInterval(Step, 1000/frameRate);
+}
+
+
 // Showcase
 let ids = document.getElementsByClassName("sh_preview");
 
